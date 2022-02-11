2c9881e054d6b34a91b5b5bd01c4e61bf07a1875
Gunnar Braun <gunnar.braun@gmx.de>
2022-02-11 18:36:46 +0100

    inital commit

---
+++ src/xcode/Example/Example/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Example
+//
+//  Created by Gunnar Braun on 11.02.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ src/xcode/Example/Example/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleApp.swift
+//  Example
+//
+//  Created by Gunnar Braun on 11.02.22.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
